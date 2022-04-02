e909d7d5e5e5d288573bab59bea3cc93c0990fbe
Call Vin <kanggara75@gmail.com>
2022-04-02 11:44:19 +0700

    add: section 1 and 2

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	content: ["index.html"],
+	theme: {
+		container: {
+			center: true,
+			padding: "70px",
+		},
+		extend: {
+			colors: {
+				secondary: "#64748b",
+				primary: "#0ea5e9",
+				dark: "#0f172a",
+			},
+			screen: {
+				"2xl": "1320px",
+			},
+		},
+	},
+	plugins: [],
+};

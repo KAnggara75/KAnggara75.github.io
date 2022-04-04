01f7c94e2e48dba9d1dfc2357c3c7ba5a5db74e3
Call Vin <kanggara75@gmail.com>
2022-04-04 16:12:06 +0700

    add: tech logo under title portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,17 @@ module.exports = {
 		},
 		extend: {
 			colors: {
+				javascript: "#F7DF1E",
 				secondary: "#64748b",
 				primary: "#0ea5e9",
+				react: "#61DAFB",
+				mysql: "#4479A1",
+				mongo: "#47A248",
 				dark: "#0f172a",
+				html: "#E34F26",
+				php: "#777BB4",
+				css: "#1572B6",
+				node: "#339933",
 			},
 			screen: {
 				"2xl": "1320px",

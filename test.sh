bb8fde4123e498c7eb4d269e57efd6c1dc78cccb
Call Vin <kanggara75@gmail.com>
2024-12-15 13:56:39 +0700

    add: Build Test

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the URL to scrape
+base_url="https://kanggara.my.id/"
+
+title=$(curl -s "$base_url" | grep -o '<script[^>]*>[^<]*</script>' | sed 's/\<script.*src="\(.*\)".*/\1/')
+
+style=$(curl -s "$base_url" | grep -o '<link[^>]*.css">' | sed 's/\<link.*href="\(.*\)".*/\1/')
+
+if [ ! -f dist/$title ]; then
+    echo "$title Not Deployed"
+fi
+
+echo $style
+
+if [ ! -f dist/$style ]; then
+    echo "$style Not Deployed"
+fi

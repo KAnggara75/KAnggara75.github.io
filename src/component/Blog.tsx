24e4d8f73478985494eecb88f6514d2e869f36b3
Call Vin <kanggara75@gmail.com>
2024-12-17 06:59:28 +0700

    fix: separate blog component

--- src/component/Blog.tsx
+++ src/component/Blog.tsx
@@ -1,3 +1,5 @@
+import PostThumb from "./PostThumb";
+
 export default function Blog() {
 	return (
 		<section
@@ -18,180 +20,18 @@ export default function Blog() {
 					</div>
 				</div>
 				<div className="flex flex-wrap">
-					<div className="w-full px-4 lg:w-1/2 xl:w-1/3">
-						<div className="bg-white rounded-xl shadow-lg overflow-hidden mb-10">
-							<img
-								src="https://source.unsplash.com/360x240?programming"
-								alt="Programming"
-								className="w-full"
-							/>
-							<div className="py-8 px-6">
-								<h3>
-									<a
-										href=""
-										className="block font-semibold text-dark text-xl hover:text-primary truncate"
-									>
-										Tips Belajar Programming
-									</a>
-								</h3>
-								<p className="font-medium text-base text-secondary mb-4">
-									Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-									Sunt, sit!
-								</p>
-								<a
-									href="#"
-									className="font-medium bg-primary text-white text-sm py-2 px-4 rounded-lg hover:opacity-80"
-								>
-									Baca Selengkapnya
-								</a>
-							</div>
-						</div>
-					</div>
-					<div className="w-full px-4 lg:w-1/2 xl:w-1/3">
-						<div className="bg-white rounded-xl shadow-lg overflow-hidden mb-10">
-							<img
-								src="https://source.unsplash.com/360x240?web"
-								alt="Programming"
-								className="w-full"
-							/>
-							<div className="py-8 px-6">
-								<h3>
-									<a
-										href=""
-										className="block font-semibold text-dark text-xl hover:text-primary truncate"
-									>
-										Tips Belajar Programming
-									</a>
-								</h3>
-								<p className="font-medium text-base text-secondary mb-4">
-									Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-									Sunt, sit!
-								</p>
-								<a
-									href="#"
-									className="font-medium bg-primary text-white text-sm py-2 px-4 rounded-lg hover:opacity-80"
-								>
-									Baca Selengkapnya
-								</a>
-							</div>
-						</div>
-					</div>
-					<div className="w-full px-4 lg:w-1/2 xl:w-1/3">
-						<div className="bg-white rounded-xl shadow-lg overflow-hidden mb-10">
-							<img
-								src="https://source.unsplash.com/360x240?backend"
-								alt="Programming"
-								className="w-full"
-							/>
-							<div className="py-8 px-6">
-								<h3>
-									<a
-										href=""
-										className="block font-semibold text-dark text-xl hover:text-primary truncate"
-									>
-										Tips Belajar Programming
-									</a>
-								</h3>
-								<p className="font-medium text-base text-secondary mb-4">
-									Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-									Sunt, sit!
-								</p>
-								<a
-									href="#"
-									className="font-medium bg-primary text-white text-sm py-2 px-4 rounded-lg hover:opacity-80"
-								>
-									Baca Selengkapnya
-								</a>
-							</div>
-						</div>
-					</div>
-					<div className="w-full px-4 lg:w-1/2 xl:w-1/3">
-						<div className="bg-white rounded-xl shadow-lg overflow-hidden mb-10">
-							<img
-								src="https://source.unsplash.com/360x240?frontend"
-								alt="Programming"
-								className="w-full"
-							/>
-							<div className="py-8 px-6">
-								<h3>
-									<a
-										href=""
-										className="block font-semibold text-dark text-xl hover:text-primary truncate"
-									>
-										Tips Belajar Programming
-									</a>
-								</h3>
-								<p className="font-medium text-base text-secondary mb-4">
-									Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-									Sunt, sit!
-								</p>
-								<a
-									href="#"
-									className="font-medium bg-primary text-white text-sm py-2 px-4 rounded-lg hover:opacity-80"
-								>
-									Baca Selengkapnya
-								</a>
-							</div>
-						</div>
-					</div>
-					<div className="w-full px-4 lg:w-1/2 xl:w-1/3">
-						<div className="bg-white rounded-xl shadow-lg overflow-hidden mb-10">
-							<img
-								src="https://source.unsplash.com/360x240?hacking"
-								alt="Programming"
-								className="w-full"
-							/>
-							<div className="py-8 px-6">
-								<h3>
-									<a
-										href=""
-										className="block font-semibold text-dark text-xl hover:text-primary truncate"
-									>
-										Tips Belajar Programming
-									</a>
-								</h3>
-								<p className="font-medium text-base text-secondary mb-4">
-									Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-									Sunt, sit!
-								</p>
-								<a
-									href="#"
-									className="font-medium bg-primary text-white text-sm py-2 px-4 rounded-lg hover:opacity-80"
-								>
-									Baca Selengkapnya
-								</a>
-							</div>
-						</div>
-					</div>
-					<div className="w-full px-4 lg:w-1/2 xl:w-1/3">
-						<div className="bg-white rounded-xl shadow-lg overflow-hidden mb-10">
-							<img
-								src="https://source.unsplash.com/360x240?devops"
-								alt="Programming"
-								className="w-full"
-							/>
-							<div className="py-8 px-6">
-								<h3>
-									<a
-										href=""
-										className="block font-semibold text-dark text-xl hover:text-primary truncate"
-									>
-										Tips Belajar Programming
-									</a>
-								</h3>
-								<p className="font-medium text-base text-secondary mb-4">
-									Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-									Sunt, sit!
-								</p>
-								<a
-									href="#"
-									className="font-medium bg-primary text-white text-sm py-2 px-4 rounded-lg hover:opacity-80"
-								>
-									Baca Selengkapnya
-								</a>
-							</div>
-						</div>
-					</div>
+					<PostThumb
+						title={"Tips Belajar Programming"}
+						href={"/blog"}
+					/>
+					<PostThumb
+						title={"Tips Belajar Programming"}
+						href={"/blog"}
+					/>
+					<PostThumb
+						title={"Tips Belajar Programming"}
+						href={"/blog"}
+					/>
 				</div>
 			</div>
 		</section>
---
+++ src/component/PostThumb.tsx
@@ -0,0 +1,37 @@
+interface PostThumbProps {
+	title: string;
+	href: string;
+}
+
+export default function PostThumb({ title, href }: Readonly<PostThumbProps>) {
+	return (
+		<div className="w-full px-4 lg:w-1/2 xl:w-1/3">
+			<div className="bg-white rounded-xl shadow-lg overflow-hidden mb-10">
+				<img
+					src="https://source.unsplash.com/360x240?programming"
+					alt={title}
+					className="w-full"
+				/>
+				<div className="py-8 px-6">
+					<h3>
+						<a
+							href={href}
+							className="block font-semibold text-dark text-xl hover:text-primary truncate"
+						>
+							{title}
+						</a>
+					</h3>
+					<p className="font-medium text-base text-secondary mb-4">
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, sit!
+					</p>
+					<a
+						href={href}
+						className="font-medium bg-primary text-white text-sm py-2 px-4 rounded-lg hover:opacity-80"
+					>
+						Baca Selengkapnya
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}

d1031908674e0151bfcd082f53139cfbbccaab30
Call Vin <kanggara75@gmail.com>
2024-12-20 13:36:28 +0700

    add: 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: "./dist",
+		emptyOutDir: true, // also necessary
+		rollupOptions: {
+			input: {
+				index: path.resolve(__dirname, "index.html"),
+				404: path.resolve(__dirname, "404.html"),
+			},
+		},
+	},
 });

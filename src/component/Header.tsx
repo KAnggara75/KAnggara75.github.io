dddabb7b1ad699a0ff2d53afeaf8f7f9add9e207
Call Vin <kanggara75@gmail.com>
2024-12-20 19:15:28 +0700

    fix: home /

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
 				<div className="flex items-center justify-between relative">
 					<div className="px-4">
 						<a
-							href="#home"
+							href="/"
 							className="font-bold text-lg text-primary block py-6"
 						>
 							Kelvin Anggara

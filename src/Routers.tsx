1cb8c9ae614cdb5a4c32decb6f946649ef33de01
Call Vin <kanggara75@gmail.com>
2024-12-22 09:36:43 +0700

    fix: header on 404

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,32 +1,30 @@
 import Home from "./pages/Home";
 import Post from "./pages/Post";
 import About from "./pages/About";
-import Header from "./component/Header";
+import ErrorNotFound from "./pages/404";
 import { Routes, Route } from "react-router";
 
 export default function Routers() {
 	return (
-		<>
-			<Header />
-			<Routes>
-				<Route
-					index
-					path="/"
-					element={<Home />}
-				/>
-				<Route
-					path="/index.html"
-					element={<Home />}
-				/>
-				<Route
-					path="about"
-					element={<About />}
-				/>
-				<Route
-					path="post/:postId"
-					element={<Post />}
-				/>
-			</Routes>
-		</>
+		<Routes>
+			<Route
+				index
+				path="/"
+				element={<Home />}
+				errorElement={<ErrorNotFound />}
+			/>
+			<Route
+				path="/index.html"
+				element={<Home />}
+			/>
+			<Route
+				path="about"
+				element={<About />}
+			/>
+			<Route
+				path="post/:postId"
+				element={<Post />}
+			/>
+		</Routes>
 	);
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,16 @@
-import About from "../component/About";
 import Blog from "../component/Blog";
-import Client from "../component/Client";
-import Contact from "../component/Contact";
-import Footer from "../component/Footer";
 import Hero from "../component/Hero";
+import About from "../component/About";
+import Client from "../component/Client";
+import Footer from "../component/Footer";
+import Header from "../component/Header";
+import Contact from "../component/Contact";
 import Portofolio from "../component/Portofolio";
 
 export default function Home() {
 	return (
 		<>
+			<Header />
 			<Hero />
 			<About />
 			<Portofolio />

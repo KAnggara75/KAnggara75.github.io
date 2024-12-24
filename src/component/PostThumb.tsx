36975877077d71cefcaf7f4376b64fec813bc30c
Call Vin <kanggara75@gmail.com>
2024-12-25 06:57:29 +0700

    fix: post thumbs data

--- src/component/PostThumb.tsx
+++ src/component/PostThumb.tsx
@@ -1,14 +1,18 @@
 interface PostThumbProps {
 	title: string;
+	subtitle: string;
 	href: string;
 	src: string;
 	img: string;
+	tags: string[];
 }
 
 export default function PostThumb({
 	title,
+	subtitle,
 	href,
 	img,
+	tags,
 }: Readonly<PostThumbProps>) {
 	return (
 		<div className="w-full px-4 lg:w-1/2 xl:w-1/3">
@@ -28,11 +32,22 @@ export default function PostThumb({
 						</a>
 					</h3>
 					<p className="mb-4 text-base font-medium text-secondary">
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, sit!
+						{subtitle}
 					</p>
+					<div className="hidden grid-cols-4 sm:grid">
+						{tags.map((tag) => (
+							<a
+								className="m-1 rounded-lg border border-transparent bg-green-600 px-2.5 py-0.5 text-sm text-white shadow-sm transition-all hover:bg-blue-600 dark:bg-amber-200 dark:text-dark hover:dark:bg-blue-600 hover:dark:text-white"
+								key={tag}
+								href={"tags/" + tag}
+							>
+								{tag.substring(0, 10)}
+							</a>
+						))}
+					</div>
 					<a
 						href={href}
-						className="rounded-lg bg-primary px-4 py-2 text-sm font-medium text-white hover:opacity-80"
+						className="mt-3 block rounded-lg bg-primary px-4 py-2 text-sm font-medium text-white hover:opacity-80"
 					>
 						Baca Selengkapnya
 					</a>
